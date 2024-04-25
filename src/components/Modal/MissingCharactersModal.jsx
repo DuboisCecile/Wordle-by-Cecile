a37c80ae941a3f83b5524626f762970dcd58f041
Cécile Dubois <duboiscecilepro@gmail.com>
2024-04-25 14:50:30 +0200

    Improved modal (added background, etc.)

--- src/components/Modal/MissingCharactersModal.jsx
+++ src/components/Modal/MissingCharactersModal.jsx
@@ -1,23 +1,20 @@
 import Modal from './Modal';
-import wantedImg from '../../assets/wanted.png';
+import wantedImg from '../../../public/images/wanted.png';
 
 export default function MissingCharactersModal({ setMissing }) {
   return (
-    <Modal open={open} onClose={() => setMissing(false)}>
-      <div className="w-96 p-10 text-center">
-        {/* <Trash size={56} className="mx-auto text-red-500" /> */}
-        <div className="flex flex-col items-center gap-5">
-          <div className="text-xl font-bold text-gray-800">Oups ! Il semble qu'il manque certains caractères !</div>
-          <img src={wantedImg} style={{ width: 200, height: 'auto' }} alt="wanted stamp" />
-          <p className="text-sm text-gray-500">Vous devez en avoir 5 !</p>
-          <button
-            className="group relative overflow-hidden rounded bg-green-500 px-5 py-2.5 text-white transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 hover:ring-2 hover:ring-green-400 hover:ring-offset-2"
-            onClick={() => setMissing(false)}
-          >
-            <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40"></span>
-            <span className="relative">OK</span>
-          </button>
-        </div>
+    <Modal open={open} onClose={() => setMissing(false)} bgClass="bg-[url('public/images/background.jpg')] bg-cover ">
+      <div className="flex w-96 flex-col items-center gap-5 bg-white bg-opacity-90 p-10 text-center">
+        <div className="text-xl font-bold">Oups ! Il semble qu'il manque certains caractères !</div>
+        <img src={wantedImg} style={{ width: 200, height: 'auto' }} alt="wanted stamp" />
+        <div className="text-xl font-bold">Vous devriez en avoir 5 !</div>
+        <button
+          className="group relative overflow-hidden rounded bg-green-500 px-5 py-2.5 text-white transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 hover:ring-2 hover:ring-green-400 hover:ring-offset-2"
+          onClick={() => setMissing(false)}
+        >
+          <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40"></span>
+          <span className="relative">OK</span>
+        </button>
       </div>
     </Modal>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,22 @@
-export default function Modal({ open, onClose, children }) {
+export default function Modal({ open, onClose, children, bgClass }) {
   return (
     // backdrop
     <div
       onClick={onClose}
-      className={`
-        fixed inset-0 flex justify-center items-center transition-colors
-        ${open ? 'visible bg-black/20' : 'invisible'}
-      `}
+      className={`fixed inset-0 flex justify-center items-center transition-colors ${open ? 'visible bg-black/65' : 'invisible'}`}
       aria-hidden="true"
     >
       {/* modal */}
       <div
         onClick={(e) => e.stopPropagation()}
-        className={`
-          bg-white rounded-xl shadow p-6 transition-all
-          ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}
-        `}
+        className={`${bgClass ? bgClass : 'bg-white'} rounded-xl shadow p-6 transition-all ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}
         aria-hidden="true"
       >
         <button
           onClick={onClose}
-          className="absolute right-2 top-2 rounded-lg bg-white p-1 text-gray-400 hover:bg-gray-50 hover:text-gray-600"
+          className="absolute right-7 top-7 rounded-lg bg-white p-1 text-gray-400 hover:bg-gray-50 hover:text-gray-600"
         >
-          X
+          <img src="../../../public/images/Xmark.svg" alt="closing X" className="w-6" />
         </button>
         {children}
       </div>
